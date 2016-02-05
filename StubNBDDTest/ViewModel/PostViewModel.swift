2c104a9bf6f725067d8e94beff0c29c49e0e4227
Anak Mirasing <thaihooligan@hotmail.com>
2016-02-05 18:04:19 +0700

    add RxCocoa and update post view model

--- StubNBDDTest/ViewModel/PostViewModel.swift
+++ StubNBDDTest/ViewModel/PostViewModel.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class PostViewModel {
+    
+    struct Constants {
+        static let baseURL = "http://jsonplaceholder.typicode.com/posts"
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    var postUserId = PublishSubject<Int?>()
+    var postTitle = PublishSubject<String?>()
+    var postBody = PublishSubject<String?>()
+    
+    var post:Post? {
+        didSet {
+            if let userId = post?.userId {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.postUserId.onNext(userId)
+                }
+            }
+            
+            if let title = post?.title {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.postTitle.onNext(title)
+                }
+            }
+            
+            if let body = post?.body {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.postBody.onNext(body)
+                }
+            }
+        }
+    }
+    
+    
+    
+}
