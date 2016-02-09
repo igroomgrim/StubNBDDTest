bff5d941576e8ef5d5f0262147756903e83724e8
Anak Mirasing <thaihooligan@hotmail.com>
2016-02-09 15:23:20 +0700

    Test sample tableview

---
+++ StubNBDDTest/View/PostCell.swift
@@ -0,0 +1,33 @@
+//
+//  PostCell.swift
+//  StubNBDDTest
+//
+//  Created by Anak Mirasing on 2/9/16.
+//  Copyright © 2016 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    // MARK: Initial Cell
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
--- StubNBDDTest/ViewController.swift
+++ StubNBDDTest/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("HELLO WORLD")
+        let pullButton = UIButton()
+        self.view.addSubview(pullButton)
+        pullButton.setTitle("Pull", forState: .Normal)
+        pullButton.backgroundColor = UIColor.brownColor()
+        pullButton.setTitleColor(UIColor.grayColor(), forState: .Highlighted)
+        pullButton.snp_makeConstraints { (make) -> Void in
+            make.width.equalTo(132)
+            make.height.equalTo(44)
+            make.left.equalTo(20)
+            make.top.equalTo(22)
+        }
+        
+        
+        let tableView = UITableView()
+        self.view.addSubview(tableView)
+        tableView.registerClass(PostCell.self, forCellReuseIdentifier: "PostCell")
+        tableView.snp_makeConstraints { (make) -> Void in
+            make.width.equalTo(self.view)
+            make.height.equalTo(self.view.snp_height).offset(-100)
+            make.top.equalTo(100)
+        }
+        
+        let disposeBag = DisposeBag()
+        
+        let items = Observable.just([
+            "First Item",
+            "Second Item",
+            "Third Item"
+            ])
+        
+        items
+            .bindTo(tableView.rx_itemsWithCellIdentifier("PostCell", cellType: PostCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = "\(element) @ row \(row)"
+            }
+            .addDisposableTo(disposeBag)
+        
+        
+        tableView
+            .rx_modelSelected(String)
+            .subscribeNext { value in
+//                DefaultWireframe.presentAlert("Tapped `\(value)`")
+                print("\(value)")
+            }
+            .addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +66,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var postViewModel = PostViewModel()
+//    var postViewModel = PostViewModel()
 }
 
